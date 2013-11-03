6dcff9479535ba173525dd855b9e79d349c62c5a
Charlotte Chang <yellowspiderlove@gmail.com>
2013-11-03 17:11:22 -0500

    rails shell no animals harmed

---
+++ railsshell/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsshell::Application.config.secret_key_base = '0959e2657d722760723f97caaccaca0a9541c540a9af01eeb216599854130d7cd4316ac14dddac7cb5879c3d3fd6292e10824d8e72ccd17fc4bc775d569287a0'
---
+++ railsshell/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsshell::Application.config.session_store :cookie_store, key: '_railsshell_session'
